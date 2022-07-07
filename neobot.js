03276327e741af3a7eace9dbfd6af6be5793485a
renzgans <108325453+renzgans@users.noreply.github.com>
2022-07-07 16:45:35 +0700

    Update neobot.js

--- neobot.js
+++ neobot.js
@@ -1,10 +1,10 @@
 /**
 
-- Neobotz Beta Multi Device
-- Created By Lexxy Official
+- Renz Beta Multi Device
+- Created By Renz Ganteng
 - Version : 3.0.0
 - Buy No Enc? Harga 150k
-- Wa gua : Wa.me/6283834558105
+- Wa gua : Wa.me/6281250365581
 
 */
 
