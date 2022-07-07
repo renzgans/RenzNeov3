0dc5527e68fded5dbf579d50812a0a9ce65fbf6c
renzgans <108325453+renzgans@users.noreply.github.com>
2022-07-07 13:13:44 +0700

    Update help.js

--- message/help.js
+++ message/help.js
@@ -10,9 +10,6 @@ return `
 exports.txtDonate = (prefix) => {
 return `
 ▷_*DONASI BOT*_
- _- DANA : 08xxxxxxxx_
- _- OVO : 08xxxxxxxx_
- _- GOPAY : 08xxxxxxxx_
- _- QRIS : https://bit.ly/3yy0qoZ_
+ _- DANA : 081250365581_
 `
 }
